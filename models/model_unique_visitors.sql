2040db8b442908047a435f588422d18a22c6e6ce
gab-santi <gabbysantiago96@yahoo.com>
2023-10-19 11:16:34 +0800

    Update unique visitor counting logic

--- models/model_unique_visitors.sql
+++ models/model_unique_visitors.sql
@@ -1,5 +1,5 @@
--- unique visitors = number of unique users, which we get via counting the anon_id
+-- unique visitors = number of unique users, which we get via counting the final_id
 -- 1358
 SELECT 
-    COUNT(DISTINCT anonymous_id) AS count_visitor
+    COUNT(DISTINCT final_id) AS count_visitor
 FROM {{ ref('model_processed_data') }}
